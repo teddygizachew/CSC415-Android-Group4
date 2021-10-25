09f0c0f80d1784b914b53944632ca2ab4d828014
khardrix <carmenandryanhuffman@yahoo.com>
2021-10-24 23:57:20 -0400

    first commit

--- app/src/main/java/com/example/groupfourproject/MainActivity.java
+++ app/src/main/java/com/example/groupfourproject/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_main);
 
         register = (TextView) findViewById(R.id.create_account);
--- app/src/main/java/com/example/groupfourproject/RegisteredUser.java
+++ app/src/main/java/com/example/groupfourproject/RegisteredUser.java
@@ -2,13 +2,60 @@ package com.example.groupfourproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class RegisteredUser extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class RegisteredUser extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView banner, createAccount;
+    private EditText etFirstName, etLastName, etEmail, etPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registered_user);
+
+        // initialize the FireBaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        createAccount = (TextView) findViewById(R.id.createAccount_button);
+        createAccount.setOnClickListener(this);
+
+        etFirstName = (EditText) findViewById(R.id.firstName_input);
+        etLastName = (EditText) findViewById(R.id.lastName_input);
+        etEmail = (EditText) findViewById(R.id.email_input_user);
+        etPassword = (EditText) findViewById(R.id.password_input_user);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar_user);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.createAccount_button:
+                createAccount();
+                break;
+        }
+    }
+
+
+    private void createAccount() {
+
     }
 }
