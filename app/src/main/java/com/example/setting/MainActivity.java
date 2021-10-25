9fef0591cf2f781ae5df46080cf3b4b9ba5339d6
LudianaA <60020563+LudianaA@users.noreply.github.com>
2021-10-24 21:53:41 -0400

    comment

--- app/src/main/java/com/example/setting/MainActivity.java
+++ app/src/main/java/com/example/setting/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,SettingsActivity.class);
         startActivity(intent);
     }
+    // setting
     
 }
