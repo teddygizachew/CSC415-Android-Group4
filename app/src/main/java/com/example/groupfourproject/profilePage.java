e672f6ac4e5581909e935cda955fa1facd2d7607
mereguno <heraldbrack@gmail.com>
2021-11-24 15:22:55 -0500

    Added Intent to move to profile screen via Floating Action Button.

--- app/src/main/java/com/example/groupfourproject/profilePage.java
+++ app/src/main/java/com/example/groupfourproject/profilePage.java
@@ -1,4 +1,14 @@
 package com.example.groupfourproject;
 
-public class profilePage {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class profilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_screen);
+    }
 }
